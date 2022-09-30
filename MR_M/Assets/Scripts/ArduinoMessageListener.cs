b60bd8d674763ea9f398e9f55369eb66690a4bd2
Hoda Ismail <hoda.ismail.96@gmail.com>
2022-09-30 17:47:48 +0200

    Slider + Arduino

--- MR_M/Assets/Scripts/ArduinoMessageListener.cs
+++ MR_M/Assets/Scripts/ArduinoMessageListener.cs
@@ -8,6 +8,7 @@
 
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 /**
  * When creating your message listeners you need to implement these two methods:
@@ -18,18 +19,22 @@ public class ArduinoMessageListener : MonoBehaviour
 {
 
     public GameObject ObjectToSpawn;
+    
+    public Slider Slider;
 
     // Invoked when a line of data is received from the serial device.
     void OnMessageArrived(string msg)
     {
         Debug.Log("Message arrived: " + msg);
+
         if (msg.Equals("Pressed"))
 		{
             Instantiate(ObjectToSpawn, new Vector3(0, 0.196f, 0), Quaternion.identity);
         } else
 		{
+            Slider.value = float.Parse(msg)/1000f;
 
-		}
+        }
         
     }
 
