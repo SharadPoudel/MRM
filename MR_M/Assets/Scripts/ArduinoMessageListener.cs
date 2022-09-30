436dac5df9d775efbb38a5a90af2bb8cea8b9caf
Hoda Ismail <hoda.ismail.96@gmail.com>
2022-09-30 17:22:29 +0200

    Adding hello world demo features
    Arduino, Leap motion sensor

---
+++ MR_M/Assets/Scripts/ArduinoMessageListener.cs
@@ -0,0 +1,46 @@
+/**
+ * Ardity (Serial Communication for Arduino + Unity)
+ * Author: Daniel Wilches <dwilches@gmail.com>
+ *
+ * This work is released under the Creative Commons Attributions license.
+ * https://creativecommons.org/licenses/by/2.0/
+ */
+
+using UnityEngine;
+using System.Collections;
+
+/**
+ * When creating your message listeners you need to implement these two methods:
+ *  - OnMessageArrived
+ *  - OnConnectionEvent
+ */
+public class ArduinoMessageListener : MonoBehaviour
+{
+
+    public GameObject ObjectToSpawn;
+
+    // Invoked when a line of data is received from the serial device.
+    void OnMessageArrived(string msg)
+    {
+        Debug.Log("Message arrived: " + msg);
+        if (msg.Equals("Pressed"))
+		{
+            Instantiate(ObjectToSpawn, new Vector3(0, 0.196f, 0), Quaternion.identity);
+        } else
+		{
+
+		}
+        
+    }
+
+    // Invoked when a connect/disconnect event occurs. The parameter 'success'
+    // will be 'true' upon connection, and 'false' upon disconnection or
+    // failure to connect.
+    void OnConnectionEvent(bool success)
+    {
+        if (success)
+            Debug.Log("Connection established");
+        else
+            Debug.Log("Connection attempt failed or disconnection detected");
+    }
+}
