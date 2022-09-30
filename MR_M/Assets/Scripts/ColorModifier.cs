88e9d287933e159d79d0b60365569ca783a30d80
SharadPoudel <sharadpoudel.info@gmail.com>
2022-09-30 14:56:12 +0200

    Interaction Pad Disabled
    Modified Object Interactions Scene!

---
+++ MR_M/Assets/Scripts/ColorModifier.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ColorModifier : MonoBehaviour
+{
+    public MeshRenderer cube;
+    public Slider red;
+    public Slider green;
+    public Slider blue; 
+
+    public void OnEdit()
+    {
+        Color color = cube.material.color;
+        color.r = red.value;
+        color.g = green.value;
+        color.b = blue.value;
+
+        cube.material.color = color;
+        cube.material.SetColor("_EmissionColor",color);
+    }
+        
+}
