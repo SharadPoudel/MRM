c6c4e281f203914076fc1966507e76b9229ccaec
Hoda Ismail <hoda.ismail.96@gmail.com>
2022-11-06 14:56:08 +0100

    Adding Glowing
    Added Glowing Effect to Arduino

--- MR_M/Assets/Scripts/ArduinoMessageListener.cs
+++ MR_M/Assets/Scripts/ArduinoMessageListener.cs
@@ -10,6 +10,7 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using System;
+using UnityEngine.Rendering.PostProcessing;
 
 /**
  * When creating your message listeners you need to implement these two methods:
@@ -20,25 +21,84 @@ public class ArduinoMessageListener : MonoBehaviour
 {
 
     public GameObject ObjectToSpawn;
-
     //public Slider Slider;
     public Text IntentValue;
+    public PostProcessVolume PostProcessVolume;
+    public Color[] Colors;
+    public double RoundedValue;
 
-    // Invoked when a line of data is received from the serial device.
-    void OnMessageArrived(string msg)
+    private Bloom Bloom;
+    private ColorParameter ColorParameter;
+    private int NumberOfRobots = 0;
+
+	private void Start()
+	{
+        Bloom = PostProcessVolume.profile.GetSetting<UnityEngine.Rendering.PostProcessing.Bloom>();
+        ColorParameter = new UnityEngine.Rendering.PostProcessing.ColorParameter();
+    }
+
+	private void Update()
+	{
+        IntentValue.text = "Intent Value = " + RoundedValue.ToString();
+
+        if (RoundedValue >= 0 && RoundedValue < 0.25)
+        {
+            ColorParameter.value = Colors[0];
+        }
+        else if (RoundedValue >= 0.25 && RoundedValue <= 0.5)
+        {
+            ColorParameter.value = Colors[1];
+        }
+        else if (RoundedValue >= 0.5 && RoundedValue < 0.75)
+        {
+            ColorParameter.value = Colors[2];
+        }
+        else
+        {
+            ColorParameter.value = Colors[3];
+        }
+
+        Bloom.color.Override(ColorParameter);
+    }
+
+	// Invoked when a line of data is received from the serial device.
+	void OnMessageArrived(string msg)
     {
         Debug.Log("Message arrived: " + msg);
 
         if (msg.Equals("Pressed"))
 		{
-            Instantiate(ObjectToSpawn, new Vector3(0, 0.196f, 0), Quaternion.identity);
-        } else
+            if (NumberOfRobots <= 6)
+            {
+                Instantiate(ObjectToSpawn, new Vector3(NumberOfRobots, 0.196f, 0), Quaternion.identity);
+                NumberOfRobots++;
+            }
+        }
+        else
 		{
-            double RoundedValue = Math.Round(float.Parse(msg) / 1000f, 1);
+            //double RoundedValue = Math.Round(float.Parse(msg) / 1000f, 1);
 
             //Slider.value = float.Parse(msg)/1000f;
             IntentValue.text = "Intent Value = " + RoundedValue.ToString();
 
+            if (RoundedValue >=0 && RoundedValue < 0.25)
+			{
+                ColorParameter.value = Colors[0];
+            }
+            else if (RoundedValue >= 0.25 && RoundedValue <= 0.5)
+            {
+                ColorParameter.value = Colors[1];
+            }
+            else if (RoundedValue >= 0.5 && RoundedValue < 0.75)
+            {
+                ColorParameter.value = Colors[2];
+            }
+            else
+            {
+                ColorParameter.value = Colors[3];
+            }
+
+            Bloom.color.Override(ColorParameter);
         }
         
     }
